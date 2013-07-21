a4ed1387a99a014d400d0405fbd3eb096b691a87
tomdov <tomdovdev@gmail.com>
2013-07-22 00:35:05 +0300

    improved the user show page and adding gravatar image

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,4 +2,10 @@ module UsersHelper
   def userNameMaxLength
     50
   end
+
+  def gravatar_for(user, options = { :size => 50 })
+    gravatar_image_tag(user.email.downcase, :alt => user.name,
+                                            :class => 'gravatar',
+                                            :gravatar => options)
+  end
 end
