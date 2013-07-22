e900afdad95ce326e28730a68d0eca0303d85d13
tomdov <tomdovdev@gmail.com>
2013-07-22 08:46:03 +0300

    adding signup failure tests and page

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -55,5 +55,32 @@ describe UsersController do
     end
   end
 
+  describe "POST 'new'" do
+
+    describe "failure" do
+
+      before(:each) do
+        @attr = { :name => "", :email => "", :password => "", :password_confirmation => "" }
+      end
+
+      it "should have the right title" do
+        post :create, :user => @attr
+        response.should have_selector('title', :content => "Sign Up")
+      end
+
+      it "should render the 'new' page" do
+        post :create, :user => @attr
+        response.should render_template('new')
+      end
+
+      it "should not create a user" do
+        lambda do
+          post :create, :user => @attr
+        end.should_not change(User, :count)
+
+      end
+    end
+  end
+
 
 end
