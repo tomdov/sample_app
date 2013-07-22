246c24918bad198a4c126b2d8fc7d6de8d8f6fd3
tomdov <tomdovdev@gmail.com>
2013-07-22 09:34:37 +0300

    finished user signup

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -80,7 +80,32 @@ describe UsersController do
 
       end
     end
+
+    describe 'success' do
+      before(:each) do
+        @attr = { :name => 'new user', :email => "user@example.com",
+                  :password => "1234", :password_confirmation => "1234" }
+      end
+
+      it "should create a user" do
+        lambda do
+          post :create, :user => @attr
+        end.should change(User, :count).by(1)
+      end
+
+      it 'should redirect to the user show page' do
+        post :create, :user => @attr
+        response.should redirect_to(user_path(assigns(:user)))
+      end
+
+      it 'should have a welcome message' do
+        post :create, :user => @attr
+        flash[:success].should =~ /welcome to the sample app/i
+      end
+    end
   end
+
+
 
 
 end
