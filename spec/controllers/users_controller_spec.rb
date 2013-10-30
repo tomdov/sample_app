c82b211f270feae5a5dd0bbf7cd5af86258a841a
tomdov <tomdovdev@gmail.com>
2013-10-30 09:34:50 +0200

    pages for following/followers

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -383,6 +383,43 @@ describe UsersController do
     end
   end
 
+  describe "follow pages" do
+    describe "when not singed in" do
+      it "should protect 'following'" do
+        get :following, :id => 1
+        response.should redirect_to(signin_path)
+      end
+
+      it "should protect 'followers'" do
+        get :followers, :id => 1
+        response.should redirect_to(signin_path)
+      end
+    end
+
+    describe "when signed in" do
+
+      before(:each) do
+        @user = test_sign_in(FactoryGirl.create(:user))
+        @another_user = FactoryGirl.create(:user,:email =>  FactoryGirl.generate(:email))
+        @user.follow!(@another_user)
+      end
+
+      it "should show user followings" do
+        get :following, :id=> @user
+        response.should have_selector('a', :href => user_path(@another_user),
+                                            :content => @another_user.name)
+      end
+
+      it "should show user followers" do
+        get :followers, :id=> @another_user
+        response.should have_selector('a', :href => user_path(@user),
+                                      :content => @user.name)
+      end
+
+    end
+  end
+
+
   describe "Content" do
     describe "Microsposts" do
       before(:each) do
